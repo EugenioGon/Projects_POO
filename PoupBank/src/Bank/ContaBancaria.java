796d06db344907ff2a32a52a219b1b63ce4cc5af
Programador Mobile <programador@sfiec.org.br>
2023-05-17 13:53:54 -0300

    Projeto Poup Bank

---
+++ PoupBank/src/Bank/ContaBancaria.java
@@ -0,0 +1,27 @@
+package Bank;
+
+public class ContaBancaria {
+//Atributos da classe
+	
+	String agencia;
+	String conta;
+	double saldo;
+	String proprietario;
+	
+//Metodos da classe 
+		
+	void depositar() {}
+
+	void sacar() {}
+
+	void transferir() {}
+	
+
+	String consultarSalto () {
+		
+		return "";
+	}
+	
+	
+
+}
---
+++ PoupBank/src/Bank/PoulpBank.java
@@ -0,0 +1,49 @@
+package Bank;
+
+import java.util.Scanner;
+
+public class PoulpBank {
+
+	public static void main(String[] args) {
+		/* Instanciando um objeto do tipo ContaBancaria e referenciando nas variavel conta1 e 
+		 * conta2
+		 */
+		
+		var conta1 = new ContaBancaria();
+		var conta2 = new ContaBancaria();
+		
+		/*
+		 * versoes mais novas podem utilizar var sem precisar colocar o nome da classe
+		 * 
+		
+		conta1.agencia = "0001";
+		conta1.conta = "137351";
+		conta1.proprietario = "Eugenio";
+		conta1.saldo = 1337.00;
+		
+		//Exibir valores atribuidos
+		 
+		System.out.println(conta1.agencia+"\n" + conta1.conta + "\n"+conta1.proprietario+"\n"+conta1.saldo);
+		
+		*/
+		
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("BEM VINDO AO POUP BANK");
+		System.out.println("CADASTRO DE CONTA");
+		System.out.print("Digite a agencia: ");
+		conta1.agencia = scn.nextLine();
+		System.out.print("Digite sua conta: ");
+		conta1.conta = scn.nextLine();
+		System.out.print("Digite o nome do proprietario: ");
+		conta1.proprietario = scn.nextLine();
+		System.out.print("Digite o saudo inicial: ");
+		conta1.saldo = scn.nextDouble();
+		
+		
+		System.out.println(conta1.agencia+"\n" + conta1.conta + "\n"+conta1.proprietario+"\n"+conta1.saldo);
+		
+		
+	}
+
+}
